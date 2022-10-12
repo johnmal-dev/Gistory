80abd46c23f8552dcd32b7d025f83128bb053337
John Malapit <johncmalapit@gmail.com>
2022-10-12 12:56:08 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='text-blue-500 text-2xl'>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
