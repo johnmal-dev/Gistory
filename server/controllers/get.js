0073e4fa2ef97b6fbb8094efb1e33b8bb07ed678
Aryse Tansy <medictansy@gmail.com>
2022-10-12 15:36:39 -0400

    refactor response

--- server/controllers/get.js
+++ server/controllers/get.js
@@ -6,20 +6,30 @@ const fetchUserController = async (req, res) => {
   await axios
     .get(`https://api.github.com/users/${user}/repos`)
     .then((response) => {
+      let owner = {
+        login: response.data[0].owner.login,
+        avatar_url: response.data[0].owner.avatar_url,
+        html_url: response.data[0].owner.html_url,
+      };
+
       let filteredList = [];
 
-      response.data.forEach(({ name, description, html_url }) => {
-        filteredList.push({
-          name: name,
-          description: description,
-          html_url: html_url,
-        });
-      });
+      response.data.forEach(
+        ({ name, description, html_url, created_at, updated_at }) => {
+          filteredList.push({
+            name,
+            description,
+            html_url,
+            created_at,
+            updated_at,
+          });
+        }
+      );
 
       res.status(200).json({
         message: "Success",
-        user,
         results: response.data.length,
+        owner,
         repos: filteredList,
       });
     })
