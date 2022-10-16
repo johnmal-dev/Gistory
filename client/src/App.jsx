ac4e308c91886919f20f4f5e5e195bc8497a91f6
John Malapit <johncmalapit@gmail.com>
2022-10-16 14:53:53 -0400

    implement getRepos function using dummy data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,42 +5,26 @@ import RepoList from './components/RepoList';
 import UserInfo from './components/UserInfo';
 import './index.css';
 
-const data = {
-  owner: {
-    login: 'Medic1111',
-    avatar_url: 'https://avatars.githubusercontent.com/u/91421539?v=4',
-    html_url: 'https://github.com/Medic1111',
-  },
-  repos: [
-    {
-      name: 'CHINGU-CHANGE',
-      description:
-        'Currency converter full stack application created as a "pair-programming" project for the Chingu.io program',
-      html_url: 'https://github.com/Medic1111/CHINGU-CHANGE',
-      created_at: '2022-07-21T16:08:21Z',
-      updated_at: '2022-07-22T16:55:38Z',
-    },
-    {
-      name: 'ARYSE-PAGANO-PORTFOLIO',
-      description: 'Aryse Tansy Personal Portfolio: Full Stack Web Developer',
-      html_url: 'https://github.com/Medic1111/ARYSE-PAGANO-PORTFOLIO',
-      created_at: '2022-06-05T20:15:48Z',
-      updated_at: '2022-07-09T21:49:00Z',
-    },
-  ],
-};
-
 function App() {
-  const [searchInput, setSearchInput] = useState('');
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div className='App bg-slate-100 px-6 py-4 flex flex-col gap-2 select-none'>
-      <h1 className='text-4xl font-bold text-gray-900'>Gistory</h1>
-      <InputForm
-        searchInput={searchInput}
-        setSearchInput={setSearchInput}
-      />
-      <UserInfo user={data.owner} />
-      <RepoList repos={data.repos} />
+    <div className='App flex flex-col gap-2 select-none'>
+      <div className='bg-slate-200 bg-opacity-90'>
+        <h1 className='mt-4 text-4xl font-bold text-gray-900'>Gistory</h1>
+        <InputForm
+          setProfile={setProfile}
+          setIsLoading={setIsLoading}
+        />
+      </div>
+      {isLoading && <div>loading</div>}
+      {profile && !isLoading && (
+        <div className="p-4">
+          <UserInfo user={profile.owner} />
+          <RepoList repos={profile.repos} />
+        </div>
+      )}
     </div>
   );
 }
--- client/src/components/InputForm.jsx
+++ client/src/components/InputForm.jsx
@@ -1,12 +1,81 @@
-import React from 'react';
+import { useState } from 'react';
 
-const InputForm = ({ searchInput, setSearchInput }) => {
+const data = {
+  owner: {
+    login: 'Medic1111',
+    avatar_url: 'https://avatars.githubusercontent.com/u/91421539?v=4',
+    html_url: 'https://github.com/Medic1111',
+  },
+  repos: [
+    {
+      name: 'CHINGU-CHANGE',
+      description:
+        'Currency converter full stack application created as a "pair-programming" project for the Chingu.io program',
+      html_url: 'https://github.com/Medic1111/CHINGU-CHANGE',
+      created_at: '2022-07-21T16:08:21Z',
+      updated_at: '2022-07-22T16:55:38Z',
+    },
+    {
+      name: 'ARYSE-PAGANO-PORTFOLIO',
+      description: 'Aryse Tansy Personal Portfolio: Full Stack Web Developer',
+      html_url: 'https://github.com/Medic1111/ARYSE-PAGANO-PORTFOLIO',
+      created_at: '2022-06-05T20:15:48Z',
+      updated_at: '2022-07-09T21:49:00Z',
+    },
+    {
+      name: 'CHINGU-CHANGE',
+      description:
+        'Currency converter full stack application created as a "pair-programming" project for the Chingu.io program',
+      html_url: 'https://github.com/Medic1111/CHINGU-CHANGE',
+      created_at: '2022-07-21T16:08:21Z',
+      updated_at: '2022-07-22T16:55:38Z',
+    },
+    {
+      name: 'ARYSE-PAGANO-PORTFOLIO',
+      description: 'Aryse Tansy Personal Portfolio: Full Stack Web Developer',
+      html_url: 'https://github.com/Medic1111/ARYSE-PAGANO-PORTFOLIO',
+      created_at: '2022-06-05T20:15:48Z',
+      updated_at: '2022-07-09T21:49:00Z',
+    },
+    {
+      name: 'CHINGU-CHANGE',
+      description:
+        'Currency converter full stack application created as a "pair-programming" project for the Chingu.io program',
+      html_url: 'https://github.com/Medic1111/CHINGU-CHANGE',
+      created_at: '2022-07-21T16:08:21Z',
+      updated_at: '2022-07-22T16:55:38Z',
+    },
+    {
+      name: 'ARYSE-PAGANO-PORTFOLIO',
+      description: 'Aryse Tansy Personal Portfolio: Full Stack Web Developer',
+      html_url: 'https://github.com/Medic1111/ARYSE-PAGANO-PORTFOLIO',
+      created_at: '2022-06-05T20:15:48Z',
+      updated_at: '2022-07-09T21:49:00Z',
+    },
+  ],
+};
+
+const InputForm = ({ setProfile, setIsLoading }) => {
+  const [searchInput, setSearchInput] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submit');
+    getRepos(searchInput);
   };
+
+  const getRepos = (id) => {
+    setIsLoading(true);
+    if (id.length) {
+      setProfile(data);
+      setTimeout(() => setIsLoading(false), 500);
+      // repoService.getRepos(id).then((res) => {
+      //   setProfile(res.data);
+      // });
+    }
+  };
+
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center mx-4'>
       <h3 className='text-lg font-medium leading-6 text-gray-900'>
         Your Github Timeline Viewer
       </h3>
@@ -17,7 +86,7 @@ const InputForm = ({ searchInput, setSearchInput }) => {
         </p>
       </div>
       <form
-        className='mt-5 sm:flex sm:items-center justify-center'
+        className='relative translate-y-6 py-6 px-10 max-w-sm sm:flex sm:items-center justify-center rounded bg-slate-700'
         onSubmit={handleSubmit}
       >
         <div className='w-full sm:max-w-xs'>
@@ -28,11 +97,12 @@ const InputForm = ({ searchInput, setSearchInput }) => {
             placeholder='Enter username here...'
             value={searchInput}
             onChange={({ target }) => setSearchInput(target.value)}
+            required
           />
         </div>
         <button
           type='submit'
-          className='mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
+          className='mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm transition'
         >
           Search
         </button>
